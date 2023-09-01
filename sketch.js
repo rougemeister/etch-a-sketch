a9effd0ff7eb0947c1e8aaffc184d9a6f6647336
rougemeister <122017023+rougemeister@users.noreply.github.com>
2023-09-01 13:25:56 +0000

    Fixed some bugs

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
+const gridContainer = document.querySelector('.grid-container');
+
+
 // grid
+
+
 function addGridBoxes(num){
     num = prompt('Enter a number to create grid eg. 20 (20x20), 40 (40x40)', 16)
     if(num < 16 || num > 100){
         alert('Choose from 16 to 100');
         return;
     }
-
-    const gridContainer = document.querySelector('.grid-container');
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     let gridBoxes = gridContainer.querySelectorAll('div');
@@ -27,7 +30,6 @@ function addGridBoxes(num){
 }
 
 function populatePad(num = 16){
-    const gridContainer = document.querySelector('.grid-container');
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
     let gridBoxes = gridContainer.querySelectorAll('div');
@@ -46,7 +48,6 @@ function populatePad(num = 16){
     }
 }
 
-populatePad();
 
 const gridButton = document.querySelector(".grid");
 gridButton.addEventListener('click', addGridBoxes);
@@ -55,10 +56,12 @@ gridButton.addEventListener('click', addGridBoxes);
 
 resetButton = document.querySelector('.reset');
 
-resetButton.addEventListener('click', (e)=>{
+resetButton.addEventListener('click', ()=>{
     const gridBoxes = document.querySelector('.grid-container').children;
     for(let box of gridBoxes){
         box.style.backgroundColor = `rgb(224, 244, 250)`;
+        box.classList.remove('rainbow-box');
+        populatePad();
     };
 })
 
@@ -70,43 +73,13 @@ rainbowButton.addEventListener('click', makeRainbow)
 
 function makeRainbow(){
     gridBoxes = document.querySelector('.grid-container').children;
-
-    function getColor (){
-        let random = Math.ceil(Math.random()*7);
-        let color = '';
-
-        switch (random){
-            case 1: 
-                color = 'teal';
-                break;
-            case 2:
-                color = 'orange';
-                break;
-            case 3:
-                color = 'rgb(51, 94, 235)';
-                break;
-            case 4:
-                color = 'yellow';
-                break;
-            case 5:
-                color = 'rgb(51, 94, 235)';
-                break;
-            case 6:
-                color = 'rgb(50, 235, 59)';
-                break;
-            case 7:
-                color = 'rgb(221, 61, 61)';
-                break;
-        }
-        return color;
+    for(let box of gridBoxes){
+        box.style.backgroundColor = `rgb(224, 244, 250)`;
     }
 
     for(let box of gridBoxes){
         box.addEventListener('mouseover', ()=>{
-            box.style.backgroundColor = getColor();
-            
-            
-          
+            box.classList.add('rainbow-box')
         })
     }
     
